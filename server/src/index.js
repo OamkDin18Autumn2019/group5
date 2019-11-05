26c9794ab4678c362c51a5e30ed203b50215c555
diegocasillas <diegocasillasdev@gmail.com>
2019-11-05 11:55:51 +0200

    Move .env file to root

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '../' });
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const httpError = require('http-errors');
