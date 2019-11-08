7e27c88d0179b8f2cde1af8a0a6c0160e81c8aeb
EetuHei <t8heee01@students.oamk.fi>
2019-11-08 16:25:41 +0200

    Team router

---
+++ server/src/team/teamRouter.js
@@ -0,0 +1,45 @@
+const { Router } = require('express');
+const httpErrors = require('http-errors');
+const { registerTeam } = require('./teamData');
+const passport = require('../config/passport/passport');
+
+const teamRouter = new Router();
+
+teamRouter.use(passport.authenticate('jwt', { session: false }));
+
+teamRouter.post('/teams', async (req, res, next) => {
+  const { name, gameId } = req.body;
+  const captainId = req.user.id;
+
+  if (name.length < 2 || name.length > 20) {
+    return next(
+      httpErrors(
+        400,
+        'Team name should be at least 2 characters and maximum of 20 characters'
+      )
+    );
+  }
+  if (isNaN(gameId)) {
+    return next(httpErrors(401, 'Game id can only be a number'));
+  }
+
+  try {
+    const teamRegister = await registerTeam(name, gameId, captainId);
+
+    if (!teamRegister) {
+      return next(httpErrors(500));
+    }
+    return res.data();
+  } catch (e) {
+    if (e.code === 'ER_DUP_ENTRY') {
+      const forbiddenError = httpErrors(
+        403,
+        `Team name '${name}' already exists`
+      );
+      return next(forbiddenError);
+    }
+    return next(e);
+  }
+});
+
+module.exports = teamRouter;
