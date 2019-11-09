0e8334937b771450ab5d8a0f3434b6d41b22540b
diegocasillas <diegocasillasdev@gmail.com>
2019-11-09 15:47:38 +0200

    Implement user model

---
+++ server/src/auth/User.js
@@ -0,0 +1,10 @@
+class User {
+  constructor({ id, username, email, fullname }) {
+    this.id = id;
+    this.username = username;
+    this.email = email;
+    this.fullname = fullname;
+  }
+}
+
+module.exports = User;
