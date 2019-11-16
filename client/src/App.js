a6fce38304b017ba21ac52bdc3adf0dc9beb4912
EetuHei <t8heee01@students.oamk.fi>
2019-11-16 18:41:29 +0200

    Added auth routes.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { inject, observer } from 'mobx-react';
 import { Switch, Route, withRouter } from 'react-router';
 import Home from './routes/Home';
+import Auth from './routes/Auth';
 
 const App = props => {
   const { appStore } = props.rootStore;
@@ -13,6 +14,8 @@ const App = props => {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/register" component={Auth} />
+      <Route exact path="/login" component={Auth} />
     </Switch>
   );
 };
