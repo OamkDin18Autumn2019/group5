5ca42e07250717f5ec10af0b8ac6bd46b200476d
EetuHei <t8heee01@students.oamk.fi>
2019-11-16 18:42:43 +0200

    functions to match backend. Register working so can start making the alert store.

--- client/src/routes/Auth/index.js
+++ client/src/routes/Auth/index.js
@@ -1,29 +1,44 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
-
+import style from './index.module.css';
 const Auth = props => {
   const { appStore, authStore } = props.rootStore;
 
   const [username, setUsername] = useState('');
+  const [usernameOrEmail, setUsernameOrEmail] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
 
   const changeUsername = e => {
     setUsername(e.target.value);
+  };
+
+  const changeEmail = e => {
+    setEmail(e.target.value);
+  };
+
+  const changeUsernameOrEmail = e => {
+    setUsernameOrEmail(e.target.value);
   };
 
   const changePassword = e => {
     setPassword(e.target.value);
   };
 
+  const changePasswordConfirmation = e => {
+    setPasswordConfirmation(e.target.value);
+  };
+
   const login = async () => {
-    await authStore.fetchToken(username, password);
+    await authStore.fetchToken(usernameOrEmail, password);
     setUsername('');
     setPassword('');
   };
 
   const register = async () => {
-    await authStore.register(username, password);
+    await authStore.register(username, email, password, passwordConfirmation);
     setUsername('');
     setPassword('');
   };
@@ -34,13 +49,46 @@ const Auth = props => {
 
   const action = props.location.pathname === '/login' ? login : register;
   const actionName =
-    props.location.pathname === '/login' ? 'Login' : 'Register';
+    props.location.pathname === '/login' ? 'Login' : 'Sign me up';
 
   return (
     <>
-      <input type="text" value={username} onChange={changeUsername} />
-      <input type="password" value={password} onChange={changePassword} />
-      <button onClick={action}>{actionName}</button>
+      <div className={style.loginBox}>
+        <div className={style.container}>
+          <h1>Sign up</h1>
+          <input
+            className={style.userInfo}
+            type="text"
+            value={username}
+            onChange={changeUsername}
+            placeholder="Username"
+          />
+          <input
+            className={style.userInfo}
+            type="text"
+            value={email}
+            onChange={changeEmail}
+            placeholder="E-mail"
+          />
+          <input
+            className={style.password}
+            type="text"
+            value={password}
+            onChange={changePassword}
+            placeholder="Password"
+          />
+          <input
+            className={style.password}
+            type="text"
+            value={passwordConfirmation}
+            onChange={changePasswordConfirmation}
+            placeholder="Retype password"
+          />
+          <button className={style.submitBtn} onClick={action}>
+            {actionName}
+          </button>
+        </div>
+      </div>
     </>
   );
 };
