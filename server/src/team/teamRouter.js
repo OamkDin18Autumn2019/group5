6d12628d13ae0e915f3a935fe8500af60ad26680
diegocasillas <diegocasillasdev@gmail.com>
2019-11-17 16:24:49 +0200

    Update register team controller to work w/ changes

--- server/src/team/teamRouter.js
+++ server/src/team/teamRouter.js
@@ -1,45 +1,32 @@
 const { Router } = require('express');
 const httpErrors = require('http-errors');
-const { registerTeam } = require('./teamData');
+const teamServices = require('./teamServices');
+const teamValidations = require('./teamValidations');
 const passport = require('../config/passport/passport');
 
 const teamRouter = new Router();
 
 teamRouter.use(passport.authenticate('jwt', { session: false }));
 
-teamRouter.post('/', async (req, res, next) => {
-  const { name, gameId } = req.body;
-  const captainId = req.user.id;
-
-  if (name.length < 2 || name.length > 20) {
-    return next(
-      httpErrors(
-        400,
-        'Team name should be at least 2 characters and maximum of 20 characters'
-      )
-    );
-  }
-  if (isNaN(gameId)) {
-    return next(httpErrors(401, 'Game id can only be a number'));
-  }
+const registerTeam = async (req, res, next) => {
+  const { knex, teamRegistrationData } = req.context;
 
   try {
-    const team = await registerTeam(name, gameId, captainId);
+    const team = await teamServices.registerTeam(knex, teamRegistrationData);
 
-    if (!team) {
-      return next(httpErrors(500));
-    }
     return res.data(201, { team });
   } catch (e) {
     if (e.code === 'ER_DUP_ENTRY') {
       const forbiddenError = httpErrors(
         403,
-        `Team name '${name}' already exists`
+        `Team name '${teamRegistrationData.name}' already exists`
       );
       return next(forbiddenError);
     }
     return next(e);
   }
-});
+};
+
+teamRouter.post('/', [teamValidations.teamRegistration, registerTeam]);
 
 module.exports = teamRouter;
