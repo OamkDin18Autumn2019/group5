8b83698b7f653550d2728116a2789a86cd2376c8
EetuHei <t8heee01@students.oamk.fi>
2019-11-18 15:25:07 +0200

    added error handling for register

--- client/src/stores/AuthStore.js
+++ client/src/stores/AuthStore.js
@@ -28,19 +28,31 @@ class AuthStore {
 
   async register(username, email, password, passwordConfirmation) {
     if (username && email && password && passwordConfirmation) {
-      const res = await fetch('http://localhost:8080/api/v1/auth/register', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          username,
-          email,
-          password,
-          passwordConfirmation
-        })
-      });
+      try {
+        const res = await fetch('http://localhost:8080/api/v1/auth/register', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            username,
+            email,
+            password,
+            passwordConfirmation
+          })
+        });
 
-      if (res.ok) {
-        await this.fetchToken(username, password);
+        if (res) {
+          const resolved = await res.json();
+          if (resolved.error) {
+            console.log(resolved.error);
+            throw new Error(resolved.error.message);
+          }
+        }
+        if (res.ok) {
+          await this.fetchToken(username, password);
+        }
+      } catch (e) {
+        console.error(e);
+        //this.rootStore.alertStore
       }
     }
   }
