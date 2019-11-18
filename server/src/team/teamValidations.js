cf9f92670bee1db40e4449df01674384dd356016
diegocasillas <diegocasillasdev@gmail.com>
2019-11-18 13:12:19 +0200

    Use express-validator for team validations

--- server/src/team/teamValidations.js
+++ server/src/team/teamValidations.js
@@ -1,23 +1,36 @@
-const teamRegistration = (req, res, next) => {
+const { body, validationResult } = require('express-validator');
+const httpError = require('http-errors');
+
+const nameCheck = body('name')
+  .trim()
+  .not()
+  .isEmpty()
+  .isLength({ min: 2, max: 20 })
+  .withMessage(
+    'Team name should be at least 2 characters and maximum of 20 characters.'
+  );
+
+const gameIdCheck = body('gameId')
+  .toInt()
+  .not()
+  .isEmpty()
+  .isInt();
+
+const validate = (req, res, next) => {
+  const errors = validationResult(req).formatWith(({ msg }) => msg);
+
+  if (!errors.isEmpty()) {
+    next(httpError(400, 'validation error', { errors: errors.mapped() }));
+  }
+
   const { name, gameId } = req.body;
   const captainId = req.user.id;
 
-  if (name.length < 2 || name.length > 20) {
-    return next(
-      httpErrors(
-        400,
-        'Team name should be at least 2 characters and maximum of 20 characters.'
-      )
-    );
-  }
-
-  if (isNaN(gameId)) {
-    return next(httpErrors(400, 'Game id can only be a number.'));
-  }
-
   req.context.teamRegistrationData = { name, gameId, captainId };
 
-  return next();
+  next();
 };
+
+const teamRegistration = [nameCheck, gameIdCheck, validate];
 
 module.exports = { teamRegistration };
