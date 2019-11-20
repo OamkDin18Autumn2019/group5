06b9fa3c475fbb748e7c7c6b07ca4d4444ec3a75
EetuHei <t8heee01@students.oamk.fi>
2019-11-20 02:20:22 +0200

    home with couple buttons and little bit of css...

--- client/src/routes/Home/index.js
+++ client/src/routes/Home/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { Link } from 'react-router-dom';
+import './index.module.css';
 
 const Home = props => {
   const { appStore } = props.rootStore;
 
   return (
     <div>
-      <h1>E-SPORTS</h1>
+      <div>
+        <h1>E-SPORTS PLATFORM</h1>
+      </div>
+      <div>
+        <Link to="/register">
+          <button>
+            <span>Register</span>
+          </button>
+        </Link>
+        <Link to="/login">
+          <button>
+            <span>Login</span>
+          </button>
+        </Link>
+        <Link to="/">
+          <button>
+            <span>Tournaments</span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
