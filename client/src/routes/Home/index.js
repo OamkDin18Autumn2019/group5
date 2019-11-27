b80346bf9c3f9bda9ecfdd79aa92b886a33bd986
EetuHei <t8heee01@students.oamk.fi>
2019-11-27 11:10:25 +0200

    basic home component

--- client/src/routes/Home/index.js
+++ client/src/routes/Home/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
+import { Link } from 'react-router-dom';
+import style from './index.module.css';
+import bgImg from '../../TI8.jpg';
 
 const Home = props => {
   const { appStore } = props.rootStore;
-
+  const sectionStyle = {
+    backgroundImage: `url(${bgImg})`
+  };
   return (
     <div>
-      <h1>E-SPORTS</h1>
+      <div className={style.mainContainer}>
+        <div style={sectionStyle} className={style.backgroundImg}>
+          <div className={style.logoContainer}>
+            <h1>Global e-Sports</h1>
+            <Link to="/login">
+              <button>
+                <span>Login</span>
+              </button>
+            </Link>
+            <Link to="/register">
+              <button>
+                <span>Register</span>
+              </button>
+            </Link>
+            <div></div>
+          </div>
+          <div className={style.container}>
+            <div className={style.btnContainer}>
+              <Link to="/tournaments">
+                <button>
+                  <span>Tournaments</span>
+                </button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
