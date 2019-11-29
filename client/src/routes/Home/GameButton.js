1df09b0941de687d9f080188e8a228215996087e
diegocasillas <diegocasillasdev@gmail.com>
2019-11-30 01:26:11 +0200

    Remove unnecessary link import

--- client/src/routes/Home/GameButton.js
+++ client/src/routes/Home/GameButton.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
-import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { NavBarLink } from './NavBar';
 
