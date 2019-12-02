3725e8496a9926e0ee09811d44a2c43a7f10eada
EetuHei <t8heee01@students.oamk.fi>
2019-12-02 13:05:51 +0200

    commented out bg img, added colors from the pallet

--- client/src/routes/Home/index.js
+++ client/src/routes/Home/index.js
@@ -5,17 +5,18 @@ import GameList from './GameList';
 import styled from 'styled-components';
 
 const StyledHome = styled.div`
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: auto;
   color: #fff;
-  font-family: Arial;
+  font-family: Robot, Sans-serif;
 `;
 
 const Section = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
-  background-image: url(https://iem.imgix.net/season-13/katowice/wp-content/uploads/2018/07/20180303_helena-kristiansson_iem-katowice_04924-1.jpg?auto=format%2Ccompress&w=6720);
+  background: #20242e;
+  // background-image: url(https://iem.imgix.net/season-13/katowice/wp-content/uploads/2018/07/20180303_helena-kristiansson_iem-katowice_04924-1.jpg?auto=format%2Ccompress&w=6720);
   background-size: cover;
   background-position: top center;
   background-repeat: no-repeat;
