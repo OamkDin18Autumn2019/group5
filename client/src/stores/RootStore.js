6369dc37a64d3d523abcc1e02bd276a5fc937411
EetuHei <t8heee01@students.oamk.fi>
2019-12-05 12:02:57 +0200

    typo

--- client/src/stores/RootStore.js
+++ client/src/stores/RootStore.js
@@ -4,7 +4,7 @@ import TeamStore from './TeamStore';
 class RootStore {
   constructor() {
     this.appStore = new AppStore(this);
-    this.TeamStore = new TeamStore(this);
+    this.teamStore = new TeamStore(this);
   }
 }
 
