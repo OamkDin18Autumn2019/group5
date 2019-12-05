bc41f787f00b67342e7200ce5e5b4523becb097e
EetuHei <t8heee01@students.oamk.fi>
2019-12-05 12:04:08 +0200

    register team functionality, dosent do anything atm if res is ok and still have to add error handling for alert

---
+++ client/src/stores/TeamStore.js
@@ -0,0 +1,42 @@
+import { decorate, action } from 'mobx';
+
+class TeamStore {
+  constructor(rootStore) {
+    this.rootStore = rootStore;
+  }
+
+  async registerTeam(name, gameId) {
+    if ((name, gameId)) {
+      try {
+        const res = await fetch('http://localhost:8080/api/v1/teams', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            name,
+            gameId
+          })
+        });
+
+        if (res) {
+          const resolved = await res.json();
+          if (resolved.error) {
+            console.log(resolved.error);
+            throw new Error(resolved.error.message);
+          }
+        }
+        if (res.ok) {
+          //do something if the team regiser works
+        }
+      } catch (e) {
+        console.error(e);
+        //this.rootStore.alertStore
+      }
+    }
+  }
+}
+
+decorate(TeamStore, {
+  registerTeam: action.bound
+});
+
+export default TeamStore;
