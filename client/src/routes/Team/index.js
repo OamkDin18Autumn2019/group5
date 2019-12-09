b26442b48817398bb56130582d17afe8081945bb
EetuHei <t8heee01@students.oamk.fi>
2019-12-09 09:58:04 +0200

    changed from css to styled components

--- client/src/routes/Team/index.js
+++ client/src/routes/Team/index.js
@@ -1,10 +1,107 @@
 import React, { useState } from 'react';
 import { inject, observer } from 'mobx-react';
 // import { Redirect } from 'react-router-dom';
-import style from './index.module.css';
+import styled from 'styled-components';
+import './index.module.css';
 
 const Team = props => {
   const { appStore, teamStore } = props.rootStore;
+
+  const MainStyle = styled.div`
+    position: relative;
+    margin: 15% auto;
+    display: flex;
+    width: 300px;
+    height: 350px;
+    background: #fff;
+    border-radius: 2px;
+    box-shadow: 0 2px 10px rgb(0, 0, 0);
+  `;
+
+  const Container = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    box-sizing: border-box;
+    padding: 40px;
+    width: 300px;
+    height: 400px;
+  `;
+
+  const Title = styled.h1`
+    margin: 10px 0 20px 0;
+    font-weight: 300;
+    font-size: 28px;
+    color: #20242e;
+  `;
+
+  const Select = styled.select`
+    font-weight: 300;
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    color: #20242e;
+    width: 220px;
+  `;
+
+  const Option = styled.option`
+    font-weight: 300;
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+    color: #20242e;
+    width: 220px;
+  `;
+
+  const Input = styled.input`
+    display: block;
+    box-sizing: border-box;
+    margin-bottom: 20px;
+    padding: 4px;
+    width: 220px;
+    height: 32px;
+    border: none;
+    border-bottom: 1px solid #aaa;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    transition: 0.2s ease;
+    &:focus {
+      border-bottom: 2px solid #20242e;
+      color: #20242e;
+      transition: 0.2s ease;
+    }
+  `;
+
+  const Button = styled.button`
+    text-decoration: none;
+    width: 220px;
+    height: 40px;
+    font-size: 16px;
+    background: #7de88c;
+    border: none;
+    color: #fff;
+    font-family: Roboto, sans-serif;
+    font-weight: 650;
+    text-transform: uppercase;
+    transition: 0.1s ease;
+    cursor: pointer;
+    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+      1px 1px 0 #000;
+    padding: 0;
+    margin: 0;
+    margin-top: 10px;
+    margin-bottom: 0;
+    &:hover,
+    &:focus {
+      opacity: 0.8;
+      box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+      transition: 0.1s ease;
+    }
+    &:active {
+      opacity: 1;
+      box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+      transition: 0.1s ease;
+    }
+  `;
 
   const [name, setName] = useState('');
   const [gameId, setGameId] = useState('1');
@@ -38,32 +135,29 @@ const Team = props => {
   return (
     <>
       {props.location.pathname === '/register-team' ? (
-        <div className={style.registerBox}>
-          <div className={style.container}>
-            <h1>Register a Team</h1>
-            <input
-              className={style.userInfo}
+        <MainStyle>
+          <Container>
+            <Title>Register a Team</Title>
+            <Input
               type="text"
               value={name}
               onChange={changeName}
               placeholder="Team name"
             />
-            <select onChange={optionId}>
-              <option label="CS:GO" value={gameId} id="1">
+            <Select onChange={optionId}>
+              <Option label="CS:GO" value={gameId} id="1">
                 CS:GO
-              </option>
-              <option lable="Dota 2" value={gameId} id="2">
+              </Option>
+              <Option lable="Dota 2" value={gameId} id="2">
                 Dota 2
-              </option>
-              <option lable="League of Legends" value={gameId} id="3">
+              </Option>
+              <Option lable="League of Legends" value={gameId} id="3">
                 League of Legends
-              </option>
-            </select>
-            <button className={style.submitBtn} onClick={action}>
-              {actionName}
-            </button>
-          </div>
-        </div>
+              </Option>
+            </Select>
+            <Button onClick={action}>{actionName}</Button>
+          </Container>
+        </MainStyle>
       ) : (
         ''
       )}
