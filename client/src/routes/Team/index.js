deee244408e1fdef247d0cfac62eb5138a14a4c2
EetuHei <t8heee01@students.oamk.fi>
2019-12-09 14:28:58 +0200

    Refactored code added styled again and fixed the re render bug, moved form to separated component

--- client/src/routes/Team/index.js
+++ client/src/routes/Team/index.js
@@ -1,6 +1,36 @@
 import React, { useState } from 'react';
 import { inject, observer } from 'mobx-react';
-import style from './index.module.css';
+import styled from 'styled-components';
+import './index.module.css';
+import StyledForm from './StyledForm';
+
+const MainStyle = styled.div`
+  position: relative;
+  margin: 15% auto;
+  display: flex;
+  width: 300px;
+  height: 330px;
+  background: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 10px rgb(0, 0, 0);
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  padding: 40px;
+  width: 300px;
+  height: 300px;
+`;
+
+const Title = styled.h1`
+  margin: 10px 0 20px 0;
+  font-weight: 300;
+  font-size: 28px;
+  color: #20242e;
+`;
 
 const Team = props => {
   const { appStore, teamStore } = props.rootStore;
@@ -19,65 +49,15 @@ const Team = props => {
     setGameId(option);
   };
 
-  const registerTeam = async () => {
-    await teamStore.registerTeam(name, gameId);
-    setName('');
-    setGameId('');
-  };
-
-  const getTeam = async () => {
-    await teamStore.getTeam();
-  };
-
-  const action =
-    props.location.pathname === '/register-team' ? registerTeam : getTeam;
-  const actionName =
-    props.location.pathname === '/register-team' ? 'submit' : '';
-
   return (
     <>
       {props.location.pathname === '/register-team' ? (
-        <div className={style.registerBox}>
-          <div className={style.container}>
-            <h1>Register a Team</h1>
-            <input
-              className={style.userInfo}
-              type="text"
-              value={name}
-              onChange={changeName}
-              placeholder="Team name"
-            />
-            <select onChange={optionId}>
-              <option
-                className={style.optionStyle}
-                label="CS:GO"
-                value={gameId}
-                id="1"
-              >
-                CS:GO
-              </option>
-              <option
-                className={style.optionStyle}
-                lable="Dota 2"
-                value={gameId}
-                id="2"
-              >
-                Dota 2
-              </option>
-              <option
-                className={style.optionStyle}
-                lable="League of Legends"
-                value={gameId}
-                id="3"
-              >
-                League of Legends
-              </option>
-            </select>
-            <button className={style.submitBtn} onClick={action}>
-              {actionName}
-            </button>
-          </div>
-        </div>
+        <MainStyle>
+          <Container>
+            <Title>Register a Team</Title>
+            <StyledForm />
+          </Container>
+        </MainStyle>
       ) : (
         ''
       )}
