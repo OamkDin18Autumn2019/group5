30ea44a38461e3df558f724ef97cdfc20c02c19c
EetuHei <t8heee01@students.oamk.fi>
2019-12-10 13:07:16 +0200

    Converted to styled

--- client/src/routes/Auth/index.js
+++ client/src/routes/Auth/index.js
@@ -1,7 +1,94 @@
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { inject, observer } from 'mobx-react';
-import style from './index.module.css';
+import styled from 'styled-components';
+import './index.module.css';
+
+const RegisterStyle = styled.div`
+  position: relative;
+  margin: 15% auto;
+  display: flex;
+  width: 300px;
+  height: 400px;
+  background: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 10px rgb(0, 0, 0);
+`;
+
+const LoginStyle = styled.div`
+  position: relative;
+  display: flex;
+  margin: 15% auto;
+  width: 300px;
+  height: 350px;
+  background: #fff;
+  border-radius: 2px;
+  box-shadow: 0 2px 10px rgb(0, 0, 0);
+`;
+
+const Container = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  box-sizing: border-box;
+  padding: 40px;
+  width: 300px;
+  height: 400px;
+`;
+
+const Title = styled.h1`
+  margin: 10px 0 20px 0;
+  font-weight: 300;
+  font-size: 28px;
+  color: #20242e;
+`;
+
+const Input = styled.input`
+  display: block;
+  box-sizing: border-box;
+  margin-bottom: 20px;
+  padding: 4px;
+  width: 220px;
+  height: 32px;
+  border: none;
+  border-bottom: 1px solid #aaa;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 400;
+  font-size: 15px;
+  transition: 0.2s ease;
+  &:focus {
+    border-bottom: 2px solid #636363;
+    color: #20242e;
+    transition: 0.2s ease;
+  }
+`;
+
+const Button = styled.button`
+  margin-top: 10px;
+  width: 220px;
+  height: 32px;
+  background: #636363;
+  border: none;
+  border-radius: 2px;
+  color: #fff;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  text-transform: uppercase;
+  transition: 0.1s ease;
+  cursor: pointer;
+  &:hover,
+  &:focus {
+    opacity: 0.8;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.4);
+    transition: 0.1s ease;
+  }
+  &:active {
+    opacity: 1;
+    box-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
+    transition: 0.1s ease;
+  }
+`;
+
 const Auth = props => {
   const { appStore, authStore } = props.rootStore;
 
@@ -51,67 +138,57 @@ const Auth = props => {
   return (
     <>
       {props.location.pathname === '/register' ? (
-        <div className={style.registerBox}>
-          <div className={style.container}>
-            <h1>Sign up</h1>
-            <input
-              className={style.userInfo}
+        <RegisterStyle>
+          <Container>
+            <Title>Sign up</Title>
+            <Input
               type="text"
               value={username}
               onChange={changeUsername}
               placeholder="Username"
             />
-            <input
-              className={style.userInfo}
+            <Input
               type="text"
               value={email}
               onChange={changeEmail}
               placeholder="E-mail"
             />
-            <input
-              className={style.password}
+            <Input
               type="password"
               value={password}
               onChange={changePassword}
               placeholder="Password"
             />
-            <input
-              className={style.password}
+            <Input
               type="password"
               value={passwordConfirmation}
               onChange={changePasswordConfirmation}
               placeholder="Retype password"
             />
-            <button className={style.submitBtn} onClick={action}>
-              {actionName}
-            </button>
-          </div>
-        </div>
+            <Button onClick={action}>{actionName}</Button>
+          </Container>
+        </RegisterStyle>
       ) : (
-        <div className={style.loginBox}>
-          <div className={style.container}>
+        <LoginStyle>
+          <Container>
             <div>
-              <h1>Login</h1>
-              <input
-                className={style.userInfo}
+              <Title>Login</Title>
+              <Input
                 type="text"
                 value={username}
                 onChange={changeUsername}
                 placeholder="Username or Email"
               />
-              <input
-                className={style.password}
+              <Input
                 type="password"
                 value={password}
                 onChange={changePassword}
                 placeholder="Password"
               />
-              <button className={style.submitBtn} onClick={action}>
-                {actionName}
-              </button>
+              <Button onClick={action}>{actionName}</Button>
             </div>
-          </div>
-        </div>
+          </Container>
+        </LoginStyle>
       )}
     </>
   );
