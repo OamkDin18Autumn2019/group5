b702647a63ac0b2ffa61f8234ccf614313b94187
diegocasillas <diegocasillasdev@gmail.com>
2019-12-10 11:22:28 +0200

    Add players to team model

--- server/src/team/Team.js
+++ server/src/team/Team.js
@@ -1,10 +1,11 @@
 class Team {
-  constructor({ id, name, gameId, captainId, recruiting }) {
+  constructor({ id, name, gameId, captainId, recruiting, players }) {
     this.id = id;
     this.name = name;
     this.gameId = gameId;
     this.captainId = captainId;
     this.recruiting = !!recruiting;
+    this.players = players;
   }
 }
 
