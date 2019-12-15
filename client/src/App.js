931467cd494dffae3a79393dadd97cd0a99c55df
Riku Heikkinen <riku.heikkinen94@gmail.com>
2019-12-15 23:44:54 +0200

    changed name to listteams

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route, withRouter } from 'react-router';
 import Home from './routes/Home';
 import Auth from './routes/Auth';
 import Alert from './components/Alert';
-import Team from './routes/Team';
+import ListTeams from './routes/ListTeams';
 import Games from './routes/Games';
 
 const App = observer(props => {
@@ -19,7 +19,7 @@ const App = observer(props => {
       {alertStore.isOpen && <Alert rootStore={props.rootStore} />}
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/teams-page" component={Team} />
+        <Route exact path="/teams-page" component={ListTeams} />
         <Route
           exact
           path="/counter-strike-global-offensive"
