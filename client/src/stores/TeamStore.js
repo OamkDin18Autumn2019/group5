ba047e5f6eea7ff37a0a2d37287d9e2b276f34b1
EetuHei <t8heee01@students.oamk.fi>
2019-12-16 13:01:12 +0200

    Added state for fetched data, state for redirecting and reload. Added fetch by team id, error handling.

--- client/src/stores/TeamStore.js
+++ client/src/stores/TeamStore.js
@@ -1,8 +1,24 @@
-import { decorate, action } from 'mobx';
+import { decorate, action, observable } from 'mobx';
 
 class TeamStore {
+  @observable teamData = [];
+  @observable redirect = false;
+  @observable reload = false;
+
   constructor(rootStore) {
     this.rootStore = rootStore;
+  }
+
+  @action setTeamData(teamData) {
+    this.teamData = teamData;
+  }
+
+  @action setRedirect(redirect) {
+    this.redirect = redirect;
+  }
+
+  @action setReload(reload) {
+    this.reload = reload;
   }
 
   async registerTeam(name, gameId) {
@@ -27,7 +43,41 @@ class TeamStore {
             console.log(resolved.error);
             throw new Error(resolved.error.message);
           }
-          console.log(resolved.data.team); // log data from response
+          if (res.ok) {
+            this.setTeamData(resolved.data.team);
+            this.setRedirect(true);
+          }
+        }
+      } catch (e) {
+        console.error(e);
+        this.rootStore.alertStore.initError(e.message);
+      }
+    }
+  }
+
+  async getTeamById(id) {
+    const { appStore } = this.rootStore;
+    if (id) {
+      try {
+        const res = await fetch(`http://localhost:8080/api/v1/teams/${id}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${appStore.accessToken}`
+          }
+        });
+
+        if (res) {
+          const resolved = await res.json();
+          console.log(resolved);
+
+          if (resolved.error) {
+            throw new Error(resolved.error.message);
+          }
+          if (res.ok) {
+            this.setTeamData(resolved.data.team);
+            this.setReload(true);
+          }
         }
       } catch (e) {
         console.error(e);
@@ -38,7 +88,8 @@ class TeamStore {
 }
 
 decorate(TeamStore, {
-  registerTeam: action.bound
+  registerTeam: action.bound,
+  getTeamById: action.bound
 });
 
 export default TeamStore;
