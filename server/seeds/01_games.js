14c13286a7786100dfae4f1d1b03397b1d25bd1d
diegocasillas <diegocasillasdev@gmail.com>
2019-12-17 18:44:22 +0200

    Add IDs to games seed

--- server/seeds/01_games.js
+++ server/seeds/01_games.js
@@ -3,11 +3,12 @@ const seed = async knex => {
 
   const games = [
     {
+      id: 1,
       name: 'Counter Strike: Global Offensive',
       slug: 'counter-strike-global-offensive'
     },
-    { name: 'DOTA 2', slug: 'dota-2' },
-    { name: 'League of Legends', slug: 'league-of-legends' }
+    { id: 2, name: 'DOTA 2', slug: 'dota-2' },
+    { id: 3, name: 'League of Legends', slug: 'league-of-legends' }
   ];
 
   return await knex('game').insert(games);
