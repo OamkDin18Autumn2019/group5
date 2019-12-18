a6c0f46a8f183e5252d5d7b3bbf62677eadd73b8
EetuHei <t8heee01@students.oamk.fi>
2019-12-18 13:34:32 +0200

    centered button text

--- client/src/routes/Games/Buttons.js
+++ client/src/routes/Games/Buttons.js
@@ -10,58 +10,62 @@ import {
 } from 'react-router-dom';
 import ListTeams from '../ListTeams';
 
+const StyledText = styled.p`
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  margin-bottom: 7px;
+  height: 25px;
+  background: #7de88c;
+  border: none;
+  color: #fff;
+  font-family: Roboto, sans-serif;
+  font-weight: 650;
+  text-transform: uppercase;
+  transition: 0.1s ease;
+  cursor: pointer;
+  text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
+    1px 1px 0 #000;
+  text-align: center;
+  margin-left: 1%;
+  margin-right: 1%;
+  margin-top: 1%;
+  @media (max-width: 700px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 150px;
+    height: auto;
+    align-items: center;
+  }
+`;
+
+const Styled = styled.div`
+  display: flex;
+  align-items: baseline;
+  flex-wrap: wrap;
+  justify-content: center;
+  text-align: center;
+  @media (max-width: 700px) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: auto;
+    margin: 0;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`;
+
 const Buttons = props => {
   const { teamStore } = props.rootStore;
   const { url } = useRouteMatch();
-  const StyledText = styled.p`
-    text-decoration: none;
-    width: 200px;
-    margin-bottom: 7px;
-    height: 25px;
-    background: #7de88c;
-    border: none;
-    color: #fff;
-    font-family: Roboto, sans-serif;
-    font-weight: 650;
-    text-transform: uppercase;
-    transition: 0.1s ease;
-    cursor: pointer;
-    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,
-      1px 1px 0 #000;
-    text-align: center;
-    margin-left: 1%;
-    margin-right: 1%;
-    margin-top: 1%;
-    @media (max-width: 700px) {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      width: 150px;
-      height: auto;
-      align-items: center;
-    }
-  `;
-
-  const Styled = styled.div`
-    display: flex;
-    align-items: baseline;
-    flex-wrap: wrap;
-    justify-content: center;
-    text-align: center;
-    @media (max-width: 700px) {
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: center;
-      width: 100%;
-      height: auto;
-      margin: 0;
-    }
-  `;
-
-  const StyledLink = styled(Link)`
-    text-decoration: none;
-  `;
 
   const getTeamData = async () => {
     await teamStore.getTeamsDataByGameId();
