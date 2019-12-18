8a75086e53e6b7a97cc096513666ba43d0d8a62c
diegocasillas <diegocasillasdev@gmail.com>
2019-12-19 00:04:20 +0200

    Seed 100 players

--- server/seeds/02_players.js
+++ server/seeds/02_players.js
@@ -3,10 +3,23 @@ const authServices = require('../src/auth/authServices');
 const seed = async knex => {
   await knex('player').del();
 
-  await authServices.registerUser(knex, {
-    username: 'root',
-    email: 'root@lmao.lol',
-    password: '111111'
+  const registerTasks = [...Array(100).keys()].map(number => {
+    return authServices.registerUser(knex, {
+      username: `player${number}`,
+      email: `player${number}@lmao.lol`,
+      password: '111111'
+    });
+  });
+
+  await knex.transaction(async trx => {
+    await Promise.all([
+      authServices.registerUser(trx, {
+        username: 'root',
+        email: 'root@lmao.lol',
+        password: '111111'
+      }),
+      ...registerTasks
+    ]);
   });
 };
 
